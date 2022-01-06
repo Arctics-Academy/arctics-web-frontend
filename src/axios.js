8ec096062325277cd9818ef69607c933bf17aa6e
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-01-06 12:32:51 +0100

    Feat: Update axios.js to request from deployed heroku app

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: "https://arctics-academic.herokuapp.com/"
+  baseURL: "https://arctics-web-app.herokuapp.com/"
 })
 
 const submitSubscriber = async (email) => {
