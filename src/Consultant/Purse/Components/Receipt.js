a800549fafcac35dee766bf9a007dff1e55168d7
羅邦齊 <b08902146@ntu.edu.tw>
2022-02-10 00:29:23 +0800

    add: consultant purse list style

--- src/Consultant/Purse/Components/Receipt.js
+++ src/Consultant/Purse/Components/Receipt.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../clt_purse.css'
 
-const testReceipt = {}
+const testReceipt = [{id: 123456, student: 'Alexar', amount: 500, paytime: '2022/02/05', consultime: '2022/02/09', paystatus: '已付款'}, {id: 123456, student: 'Alexar', amount: 500, paytime: '2022/02/05', consultime: '2022/02/09', paystatus: '已付款'}]
 
 const DisplayMoney = ({title, amount}) => {
     return (
@@ -15,14 +15,14 @@ const DisplayMoney = ({title, amount}) => {
 
 const displayHeader = () => {
     return (
-        <div className='clt_purse-list-header'>
-            <span className='clt_purse_list-header-id'>訂單編號↓</span>
-            <span className='clt_purse_list-header-student'>學生姓名</span>
-            <span className='clt_purse_list-header-amount'>金額↓</span>
-            <span className='clt_purse_list-header-pay-time'>付款時間↓</span>
-            <span className='clt_purse_list-header-consul-time'>諮詢時間↓</span>
-            <span className='clt_purse_list-header-pay-status'>提領狀態↓</span>
-        </div>
+        <>
+            <span className='clt_purse-list-header-id'>訂單編號↓</span>
+            <span className='clt_purse-list-header-student'>學生姓名</span>
+            <span className='clt_purse-list-header-amount'>金額↓</span>
+            <span className='clt_purse-list-header-pay-time'>付款時間↓</span>
+            <span className='clt_purse-list-header-consul-time'>諮詢時間↓</span>
+            <span className='clt_purse-list-header-pay-status'>提領狀態↓</span>
+        </>
     )
 }
 
@@ -31,12 +31,12 @@ const displayList = (data) => {
         data.map((e) => {
             return (
                 <div className='clt_purse-list-item'>
-                    <span className='clt_purse_list-item-id'>{e.id}</span>
-                    <span className='clt_purse_list-item-student'>{e.student}</span>
-                    <span className='clt_purse_list-item-amount'>{e.amount}</span>
-                    <span className='clt_purse_list-item-pay-time'>{e.paytime}</span>
-                    <span className='clt_purse_list-item-consul-time'>{e.consultime}</span>
-                    <span className='clt_purse_list-item-pay-status'>{e.paystatus}</span>
+                    <span className='clt_purse-list-item-id'>{e.id}</span>
+                    <span className='clt_purse-list-item-student'>{e.student}</span>
+                    <span className='clt_purse-list-item-amount'>{e.amount}</span>
+                    <span className='clt_purse-list-item-pay-time'>{e.paytime}</span>
+                    <span className='clt_purse-list-item-consul-time'>{e.consultime}</span>
+                    <span className='clt_purse-list-item-pay-status'>{e.paystatus}</span>
                 </div>
             )
         })
@@ -48,7 +48,7 @@ const Receipt = () => {
     return (
         <div className='clt_purse-receipt-body'>
             <div className='clt_purse-display-amount'>
-                <div className='clt_purse-receipt-display-avaiable'>
+                <div className='clt_purse-receipt-display-available'>
                     <DisplayMoney title={'可提領金額'} amount={1000} />
                 </div>
                 <div className='clt_purse-receipt-display-unwithdrawed'>
