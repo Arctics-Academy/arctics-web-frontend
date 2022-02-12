5d548b3b02abf6e108fff8034b141dbccb9dd113
GinyWang <giny.wang@shopee.com>
2022-02-12 20:32:59 +0800

    add: consultant-profile

--- src/Consultant/Profile/Components/Intro.js
+++ src/Consultant/Profile/Components/Intro.js
@@ -1,6 +1,13 @@
-import "../clt_profile.css";
+import Avatar from "../img/tmp_avatar.png";
+import Star from "../img/star.svg";
+import Pen from "../img/edit-pen.svg";
+import Photo from "../img/upload-photo.svg";
+import "./intro.css";
 
 const tempIntro = {
+  name: "梁芮瑄",
+  times: 5,
+  star: 4.8,
   fee: 250,
   education: {
     school: "國立台灣大學",
@@ -9,39 +16,107 @@ const tempIntro = {
   },
   disciplines: ["語言文學"],
   items: ["面試技巧", "面試技巧", "面試技巧", "面試技巧"],
-  experiences: [
-    "國立政治大學 日文系 備取",
-    "東吳大學 日文系 正取",
-    "TOEIC 聽讀 金色證書",
-    "JEPT N2 通過 (178分)",
-  ],
+  experiences:
+    "國立政治大學 日文系 備取\n東吳大學 日文系 正取\nTOEIC 聽讀 金色證書\nJEPT N2 通過 (178分)",
   intro:
     "我也不知道可以寫什麼，大概請他們寫一些諮詢風格、諮詢經歷、教學理念、簡述自己的升學歷程之類的吧......嗎？",
 };
 
+const showBlocks = (items) => {
+  return items.map((e) => {
+    return <span class="intro-main-block">{e}</span>;
+  });
+};
+
 const Intro = () => {
   return (
-    <div class="clt_home-intro">
-      <div>
-        <span>費用</span>
-        <span>{tempIntro.fee}</span>
+    <div class="intro">
+      <div class="intro-top">
+        <img class="intro-top-avatar" src={Avatar} alt="my avatar" />
+        <img class="intro-top-photo" src={Photo} alt="my avatar" />
       </div>
-      <div>
-        <span>就讀/畢業</span>
-        <span>{tempIntro.education.school}</span>
-        <span>{tempIntro.education.major}</span>
-        <span>{tempIntro.education.grade}</span>
+      <div class="intro-top">
+        <span class="intro-top-name">{tempIntro.name}</span>
+
+        <div>
+          <span class="intro-top-already">已諮詢</span>
+          <span class="intro-top-times">{tempIntro.times}次</span>
+        </div>
+        <div>
+          <span>
+            <img class="intro-top-star" src={Star} alt="star" />
+          </span>
+          <span class="intro-top-point">{tempIntro.star}</span>
+        </div>
       </div>
-      <div>
-        <span>學群</span>
-        <span>{tempIntro.fee}</span>
+
+      <div class="intro-top">
+        <button class="intro-top-edit">
+          <img src={Pen} alt="star" />
+          編輯個人檔案
+        </button>
+
+        <button class="intro-top-student">
+          <img src={Pen} alt="star" />
+          學生角度檢視
+        </button>
       </div>
-      <div>
-        <span>諮詢項目</span>
-      </div>
-      <div>
-        <span>相關經歷/能力證明</span>
-      </div>
+
+      <div class="Line-2"></div>
+
+      <table class="intro-main">
+        <tr>
+          <td>
+            <span class="intro-main-key">費用</span>
+          </td>
+          <td>
+            <span class="intro-main-text">{tempIntro.fee}</span>
+            <span class="intro-main-unit">/半小時</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span class="intro-main-key">就讀/畢業</span>
+          </td>
+          <td>
+            <span class="intro-main-text">
+              {tempIntro.education.school}
+              <br />
+              {tempIntro.education.major}
+              <br />
+              {tempIntro.education.grade}
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span class="intro-main-key">學群</span>
+          </td>
+          <td>{showBlocks(tempIntro.disciplines)}</td>
+        </tr>
+        <tr>
+          <td>
+            <span class="intro-main-key">諮詢項目</span>
+          </td>
+          <td>{showBlocks(tempIntro.items)}</td>
+        </tr>
+        <tr>
+          <td>
+            <span class="intro-main-key">相關經歷/ 能力證明</span>
+          </td>
+          <td>
+            <span class="intro-main-text">{tempIntro.experiences}</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span class="intro-main-key">個人簡介</span>
+          </td>
+          <td>
+            <span class="intro-main-text">{tempIntro.intro}</span>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 };
--- src/Consultant/Profile/Container/ConsulProfile.js
+++ src/Consultant/Profile/Container/ConsulProfile.js
@@ -1,43 +1,21 @@
-import Info from "./Info";
-import Base from "../Components/Base";
-import { styled } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import Intro from "../Components/Intro";
+
 import "./ConsulProfile.css";
-
-const MyButton = styled(Button)({
-  boxShadow: "none",
-  textTransform: "none",
-  fontSize: 16,
-  padding: "6px 12px",
-  border: "1px solid",
-  lineHeight: 1.5,
-  backgroundColor: "#0063cc",
-  borderColor: "#0063cc",
-
-  "&:hover": {
-    backgroundColor: "#0069d9",
-    borderColor: "#0062cc",
-    boxShadow: "none",
-  },
-  "&:active": {
-    boxShadow: "none",
-    backgroundColor: "#0062cc",
-    borderColor: "#005cbf",
-  },
-  "&:focus": {
-    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
-  },
-});
 
 const ConsulProfile = () => {
   return (
     <div class="main">
-      <span class="title">個人檔案</span>
-      <div class="Line-1"></div>
-      <div class="menu">
-        <button>基本資料</button>
-        <button>帳戶設定</button>
-        <button>時間表</button>
+      <div>
+        <span class="title">個人檔案</span>
+        <div class="Line-1"></div>
+        <div class="menu">
+          <button>基本資料</button>
+          <button>帳戶設定</button>
+          <button>時間表</button>
+        </div>
+      </div>
+      <div>
+        <Intro />
       </div>
     </div>
   );
