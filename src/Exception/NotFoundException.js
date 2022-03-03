b0f0da8713810fc64e2a43c87a5e9ae29d8b7a01
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-03 21:58:11 +0000

    fix: update title for 404 page

--- src/Exception/NotFoundException.js
+++ src/Exception/NotFoundException.js
@@ -1,9 +1,14 @@
 import './exception.css'
 import { ReactComponent as Background } from './img/not-found-background.svg'
 
+import MetaTags from 'react-meta-tags'
+
 const NotFoundException = () => {
     return (
         <div className='exp_not-found-main'>
+            <MetaTags>
+                <title>找不到此頁面 | Arctics</title>
+            </MetaTags>
             <Background alt='背景圖片' className='exp_not-found-background' />
             <div>
                 <h1 className='exp_not-found-heading'>404</h1>
