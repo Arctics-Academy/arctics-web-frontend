4fa77eb22b0ee164551a78889dd76d5288db1df7
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-03 21:53:33 +0000

    chore: update meta tags for landing page

--- src/LandingPage/Containers/LandingPage.js
+++ src/LandingPage/Containers/LandingPage.js
@@ -5,9 +5,15 @@ import Function from '../Components/Function';
 import ContactUs from '../Components/ContactUs';
 import LeaderIntro from '../Components/LeaderIntro';
 
+import MetaTags from 'react-meta-tags';
+
 function LandingPage() {
   return (
     <>
+      <MetaTags>
+        <title>升大學顧問媒合平台 | Arctics</title>
+        <meta name='description' content='Arctics是線上顧問媒合平台，讓高中生透過與大學生一對一線上視訊諮詢的方式，以最有效率的方式找尋自己的目標並掌握升學方式。' />
+      </MetaTags>
       <Header />
       <main>
         <AboutUs />
