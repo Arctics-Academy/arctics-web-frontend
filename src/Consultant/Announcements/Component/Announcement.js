1f11e2be5beb428b55f431f6610bad4b948ae8c7
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-04 19:52:15 +0000

    fix: fix consultant announcement css

--- src/Consultant/Announcements/Component/Announcement.js
+++ src/Consultant/Announcements/Component/Announcement.js
@@ -4,49 +4,46 @@ import { useState, useContext } from "react";
 
 const announce = [
   {
-    title: "【問卷調查】",
-    main: "顧客問卷抽大獎",
-    content: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    title: "[問卷調查] 顧客問卷抽大獎",
+    main: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    read: true,
     time: "2021/08/17（二）15:34",
   },
   {
-    title: "【問卷調查】",
-    main: "推薦朋友來",
-    content: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    title: "[問卷調查] 推薦朋友來",
+    main: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    read: false,
     time: "2021/08/17（二）15:34",
   },
   {
-    title: "【問卷調查】",
-    main: "推薦朋友來",
-    content: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    title: "[問卷調查] 推薦朋友來",
+    main: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    read: false,
     time: "2021/08/17（二）15:34",
   },
   {
-    title: "【平台快訊】",
-    main: "維修公告",
-    content: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    title: "[平台快訊] 維修公告",
+    main: "內文內文內文內文內文內文內文內文內文內文內文內文內文……",
+    read: false,
     time: "2021/08/17（二）15:34",
   },
 ];
 
 const announcements = announce.map( (obj, idx)=>{
   return(
-    <div className="clt_announce-line-wrapper" key={idx}>
-      <p className="clt_announce-line-title">{obj.title}</p>
-      <p className="clt_announce-line-main">{obj.main}</p>
-      <p className="clt_announce-line-content">{obj.content}</p>
-      <p className="clt_announce-line-time">{obj.time}</p>
-      <div className="clt_announce-line-border"></div>
+    <div className={(obj.read ? "clt_notif-line-wrapper clt_notif-line-wrapper-read" : "clt_notif-line-wrapper")} key={idx}>
+      <p className="clt_notif-line-content"><span className={(obj.read ? "clt_notif-line-title-read" : "clt_notif-line-title-unread")}>{obj.title}</span><span className={(obj.read ? "clt_notif-line-main-read" : "clt_notif-line-main-unread")}>&nbsp;&nbsp;&#8212;&nbsp;&nbsp;{obj.main}</span></p>
+      <p className="clt_notif-line-time">{obj.time}</p>
     </div>
   );
   
 } )
 const Announcement = () => {
   return (
-    <div className="clt_announce-wrapper">
+    <div className="clt_notif-wrapper">
       <img src={icon} alt="icon"/>
-      <span className="clt_announce-head">最新公告</span>
-      <div className="clt_announce-display-frame">
+      <span className="clt_notif-head">最新公告</span>
+      <div className="clt_notif-display-frame">
         {announcements}
       </div>
     </div>
--- src/Consultant/Announcements/Container/ConsulAnnounce.js
+++ src/Consultant/Announcements/Container/ConsulAnnounce.js
@@ -2,10 +2,15 @@ import { useState } from "react";
 import Announcement from "../Component/Announcement";
 import Notification from "../Component/Notification";
 
+import { MetaTags } from "react-meta-tags";
+
 const ConsulAnnounce = () => {
   return (
     <div className="clt_announce-main">
-      <Announcement />;
+      <MetaTags>
+        <title>通知 | Arctics</title>
+      </MetaTags>
+      <Announcement />
       <Notification />
     </div>
   );
