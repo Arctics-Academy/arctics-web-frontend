aa6a29d9be919b151a60bfcbc1465dc6776cce8b
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-04 21:10:06 +0000

    fix: futher fix nav bar issues

--- src/GlobalComponents/Nav.js
+++ src/GlobalComponents/Nav.js
@@ -50,24 +50,24 @@ export default function Nav() {
 		    		<div className="nav__right">
 			    		<ul className="nav__menu">
 				   		 	<li className="nav__menu-item" onClick={() => {console.log(context.Info)}}><Link to='/consultant-home'>首頁</Link></li>
-				   		 	<Link to='/consultant-announcement'><li className="nav__menu-item">
-									<a>
+				   		 	<li className="nav__menu-item">
+								<Link to='/consultant-announcement'><a>
 									通知 <span className='nav__notify-count'>9</span>
-									</a>
-							</li></Link>
-				   		 	<li className="nav__menu-item"><a className='nav__menu-submenu-link'>我的諮詢</a>
+									</a></Link>
+							</li>
+				   		 	<li className="nav__menu-item"><a>我的諮詢</a>
 								<ul className='nav__menu-submenu'>
 									<Link to='/consultant-schedule/calender'><li className='nav__menu-submenu-item'>行事曆</li></Link>
 									<Link to='/consultant-schedule/list'><li className='nav__menu-submenu-item'>所有諮詢</li></Link>
 									{/* <li className='nav__menu-submenu-item'>請假</li> */}
 								</ul>
 							</li>
-							<Link to='/consultant-purse/receipt'><li className="nav__menu-item"><a className='nav__menu-submenu-link'>我的錢包</a></li></Link>
+							<Link to='/consultant-purse/receipt'><li className="nav__menu-item"><a className='nav__menu-menu-item'>我的錢包</a></li></Link>
 								{/* <ul className='nav__menu-submenu'>
 									<Link to='/consultant-purse/receipt'><li className='nav__menu-submenu-item'>帳戶明細</li></Link>
 									<li className='nav__menu-submenu-item'>提領</li>
 								</ul> */}
-				   		 	<li className="nav__menu-item" ><a className='nav__menu-submenu-link' >小幫手</a></li>
+				   		 	{/* <li className="nav__menu-item" ><a>小幫手</a></li> */}
 								{/* <ul className='nav__menu-submenu'>
 									<li className='nav__menu-submenu-item'>常用題庫</li>
 									<li className='nav__menu-submenu-item'>網站導覽</li>
