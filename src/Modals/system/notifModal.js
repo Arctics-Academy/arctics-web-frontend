9b2dece284656fdc2e7712d6fa0da19ea3f1e2d4
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-05 01:21:00 +0000

    fix: fix notification modal center issue

--- src/Modals/system/notifModal.js
+++ src/Modals/system/notifModal.js
@@ -3,8 +3,8 @@ import './system_modal.css'
 
 const NotifModal = ({title, content, hidden, setHidden}) => {
     return (
-        <div className='sys_modal-open-meeting' style={hidden? {display:'none'}:{}}>
-            <div className='sys_modal-open-meeting-info'>
+        <div className='sys_modal-notif' style={hidden? {display:'none'}:{}}>
+            <div className='sys_modal-notif-info'>
                 <div className='sys_modal-notif-icon-area'>
                     <p className='sys_modal-notif-subheading'>{title}</p>
                     <p className='sys_modal-notif-text'>{content}</p>
