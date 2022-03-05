d0283c8a27ea0ecce8c8dfa2adec4c1597f42672
羅邦齊 <b08902146@ntu.edu.tw>
2022-03-05 10:37:28 +0800

    remove: auth for temp usage

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ import InternalServerErrorException from './Exception/InternalServerErrorExcepti
 import Login from './Login/Login';
 import StudentHome from './Student/Home/Container/StudentHome';
 import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute';
 import './style.css';
 //import './responsive.css';
 import RegisterMobileOTP from './Register/RegisterMobileOTP';
@@ -41,7 +40,7 @@ const App = () => {
     if (id.subString(0, 2) === 'TR') return 'consultant'
     else return 'student'
   }
-
+  /*
   useEffect(async () => {
     console.log('reload')
     const { status, data, message } = await authFetchAllData();
@@ -50,12 +49,12 @@ const App = () => {
       setAuth(true)
       context.setLogin(true)
       context.setInfo('login', wrapLoginData(data, getIdentity(data.id)))
-      console.log(context.isLogin, context.Info)
       history.push('/consultant-home')
     } else {
       setAuth(false)
     }
   }, [])
+  */
 
   return (
     <>
@@ -67,24 +66,24 @@ const App = () => {
           <Route exact path="/login" component={Login} />
           <Route exact path="/register-identity" component={RegisterIdentity} />
           <Route exact path="/register/:identity" component={Register} />
-          <ProtectedRoute exact path="/register-mobile-otp" auth={auth} component={RegisterMobileOTP} />
-          <ProtectedRoute exact path="/register-email-otp" auth={auth} component={RegisterEmailOTP} />
-          <ProtectedRoute exact path='/register-success' auth={auth} component={RegisterSuccess} />
-          <ProtectedRoute exact path="/consultant-home" auth={auth} component={ConsulHome} />
-          <ProtectedRoute exact path="/consultant-profile" auth={auth} component={ConsulProfile} />
-          <ProtectedRoute exact path="/consultant-schedule/:mode" auth={auth} component={ConsulSchedule} />
-          <ProtectedRoute exact path="/consultant-purse/:mode" auth={auth} component={ConsulPurse} />
-          <ProtectedRoute exact path="/consultant-success-cancel" auth={auth} component={ConsulCancelSuccess} />
-          <ProtectedRoute exact path="/consultant-multi-cancel" auth={auth} component={ConsulMultiCancel} />
-          <ProtectedRoute exact path="/consultant-announcement" auth={auth} component={ConsulAnnounce} />
-          <ProtectedRoute exact path="/student-home" auth={auth} component={StudentHome} />
-          <ProtectedRoute exact path="/modal-test" auth={auth} component={OpenMeetingModal} />
-          <ProtectedRoute exact path="/profile-photo-modal" auth={auth} component={ProfilePhotoModal} />
-          <ProtectedRoute exact path="/empty-function-modal" auth={auth} component={EmptyFunctionModal} />
-          <ProtectedRoute exact path="/notif-modal" component={NotifModal} />
+          <Route exact path="/register-mobile-otp" component={RegisterMobileOTP} />
+          <Route exact path="/register-email-otp" component={RegisterEmailOTP} />
+          <Route exact path='/register-success' component={RegisterSuccess} />
+          <Route exact path="/consultant-home" component={ConsulHome} />
+          <Route exact path="/consultant-profile" component={ConsulProfile} />
+          <Route exact path="/consultant-schedule/:mode" component={ConsulSchedule} />
+          <Route exact path="/consultant-purse/:mode" component={ConsulPurse} />
           <Route exact path="/exception/404" component={NotFoundException} />
           <Route exact path="/exception/500" component={InternalServerErrorException} />
           <Redirect from='*' to='/exception/404' />
+          <Route exact path="/consultant-announcement" component={ConsulAnnounce} />
+          <Route exact path="/consultant-success-cancel" component={ConsulCancelSuccess} />
+          <Route exact path="/consultant-multi-cancel" component={ConsulMultiCancel} />
+          <Route exact path="/student-home" component={StudentHome} />
+          <Route exact path="/modal-test" component={OpenMeetingModal} />
+          <Route exact path="/profile-photo-modal" component={ProfilePhotoModal} />
+          <Route exact path="/empty-function-modal" component={EmptyFunctionModal} />
+          <Route exact path="/notif-modal" component={NotifModal} />
         </Switch>
       </div>
     </>
