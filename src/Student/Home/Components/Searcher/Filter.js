06eb777dc78c92e4c30194bbb1193eb58343a853
Sean <72684085+Bungeeee@users.noreply.github.com>
2022-04-24 22:04:32 +0800

    add: filter field to subject mapping

--- src/Student/Home/Components/Searcher/Filter.js
+++ src/Student/Home/Components/Searcher/Filter.js
@@ -5,12 +5,16 @@ import { ReactComponent as HashtagIcon } from '../../img/hashtag.svg'
 import { ReactComponent as SchoolIcon } from '../../img/school.svg'
 import { ReactComponent as ExpIcon } from '../../img/exp.svg'
 import { ReactComponent as BachelorIcon } from '../../img/bachelor.svg'
+import { NTUFieldToMajorMap } from './Mapping'
+import { useState } from 'react'
 
 const experienceHashtags = ['自然組', '社會組', '轉組', '特殊班', '校隊', '社團', '營隊', '畢聯會', '有補習', '沒補習', '自學', '學生會', '競賽']
 const admissionHashtags = ['特殊選材', '繁星推薦', '個人申請', '指考分發', '學習歷程', '模擬面試', '面試技巧', '二階筆試', '認識校系']
+const fields = ['文史哲學群', '外語學群', '社會心理學群', '資訊學群', '藝術學群', '數理化學群', '地球環境學群', '法政學群', '財經學群', '醫藥衛生學群', '工程學群', '生物資源學群', '大眾傳播學群', '管理學群', '生命科學學群']
 
 const Filter = () => {
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const [selectedField, setField] = useState('')
     const searcher = useForm()
     const displayDays = () => {
         const sevenDays = ['日', '一', '二', '三', '四', '五', '六']
@@ -37,6 +41,19 @@ const Filter = () => {
         })    
     }
 
+    const filedOptions = (list) => {
+        return list.map((e) => {
+            return <option className='std_sf_option' value={e}>{e}</option>
+        })
+    }
+
+    const majorOptions = (field) => {
+        if (field === '' || field[0] === undefined) return []
+        return NTUFieldToMajorMap[field].map((e) => {
+            return <option className='std_sf_option' value={e} >{e}</option>
+        })
+    }
+
     const onSubmit = (data) => {
         console.log(data)
     }
@@ -49,13 +66,13 @@ const Filter = () => {
                 </div>
                 <form className='std_filter-sf-selects' onSubmit={searcher.handleSubmit(onSubmit)}>
                     <BachelorIcon className='std_filter-sf-icon' />
-                    <select className='std_filter-sf-select' id='field' {...searcher.register('field')}>
+                    <select className='std_filter-sf-select' id='field' {...searcher.register('field')} onChange={(e)=>{setField(e.target.value)}}>
                         <option className='std_sf_option' disabled selected>選擇學群領域</option>
-                        <option className='std_sf_option' >CS/IT</option>
+                        {filedOptions(fields)}
                     </select>
-                    <select className='std_filter-sf-select' id='subject' {...searcher.register('subject')}>
-                        <option className='std_sf_option' disabled selected>選擇學門</option>
-                        <option className='std_sf_option' >Computer Science</option>
+                    <select className='std_filter-sf-select' id='subject' {...searcher.register('major')}>
+                        <option className='std_sf_option' disabled selected>選擇學系</option>
+                        {majorOptions(selectedField)}
                     </select>
                     <button type='submit' className='std_filter_temp_submit'>搜尋</button>
                 </form>
