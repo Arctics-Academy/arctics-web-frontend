028434420a421670e320c24ab4c3ab9812f05489
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-07-02 12:25:11 +0100

    chore: copy consultant schedule over to student

---
+++ src/Student/Schedule/Components/Calender.js
@@ -0,0 +1,104 @@
+import { useState, useContext } from 'react'
+import DateRow from "./DateRow"
+import rightArrow from "../img/rightArrow.png"
+import leftArrow from "../img/leftArrow.png"
+import { ParamContext } from '../../../ContextReducer'
+import { buildMonthArr } from '../../../DataProcessUtils'
+
+/* calender should be a 7 * 6 array 
+   format:
+   [[{date: Int, meetings: [{time:..., status:...}...]...]..]
+   {}(empty object) in calneder means empty date on calender => month view
+*/
+
+/* above => test data */
+const monthMap = {
+    1: '1月', 2: '2月', 3: '3月', 4: '4月', 5: '5月', 6: '6月', 7: '7月', 8: '8月', 9: '9月', 10: '10月', 11: '11月', 12: '12月'
+}
+
+const showTitle = (year, month) => {
+    return `${year} ${monthMap[month]}`
+}
+
+const showHeader = () => {
+    const weekday = ['日', '一', '二', '三', '四', '五', '六']
+    return (
+        weekday.map((e) => (
+            <div className='clt_schedule-calender-header-text'>
+                <span>{e}</span>
+            </div>
+        ))
+    )
+}
+
+const Calender = () => {
+    let curDate = new Date()
+    const [year, setYear] = useState(2022)
+    const [month, setMonth] = useState(curDate.getMonth()+1)
+    const context = useContext(ParamContext)
+    const handleAddMonth = () => {
+        let m = month, y = year
+        if (month === 12) {
+            m = 1
+            y++
+        } else {
+            m++
+        }
+        if (context.Info.meetingsByTime[y] === undefined || context.Info.meetingsByTime[y][m] === undefined) {
+            let temp = context.Info.meetingsByTime
+            if (context.Info.meetingsByTime[y] === undefined) temp[y] = {}
+            temp[y][m] = buildMonthArr(y, m)
+            context.setLogin(true)
+            context.setInfo({
+                type: 'addMonthForView',
+                payload: temp
+            })
+        }
+        setMonth(m)
+        setYear(y)
+    }
+    const handleMinusMonth = () => {
+        let m = month, y = year
+        if (month === 1) {
+            m = 12
+            y--
+        } else {
+            m--
+        }
+        if (context.Info.meetingsByTime[y] === undefined || context.Info.meetingsByTime[y][m] === undefined) {
+            let temp = context.Info.meetingsByTime
+            if (context.Info.meetingsByTime[y] === undefined) temp[y] = {}
+            temp[y][m] = buildMonthArr(y, m)
+            context.setInfo({
+                type: 'addMonthForView',
+                payload: temp
+            })
+            context.setLogin(true)
+        }
+        setMonth(m)
+        setYear(y)
+    }
+    return (
+        <>
+            <div className="clt_schedule-calender">
+                <div className='clt_schedule-calender-title'>
+                    <button className='clt_schedule-calender-btn-left' onClick={handleMinusMonth}>
+                        <img className='clt_schedule-calender-title-left-arrow' src={leftArrow}/>
+                    </button>
+                    <span className='clt_schedule-calender-title-text'>{showTitle(year, month)}</span>
+                    <button className='clt_schedule-calender-btn-left' onClick={handleAddMonth}>
+                        <img className='clt_schedule-calender-title-right-arrow' src={rightArrow}/>
+                    </button>
+                </div>
+                <div className='clt_schedule-calender-header'>
+                    {showHeader()}
+                </div>
+                <div className='"clt_schedule-calnder-content'>
+                    {(context.Info.meetingsByTime[year]===undefined)? []:context.Info.meetingsByTime[year][month].map((r) => (<DateRow row={r} />))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Calender
---
+++ src/Student/Schedule/Components/MeetingList.js
@@ -0,0 +1,97 @@
+import { useState, useContext } from 'react'
+import meeting from './ListComponent'
+import { ParamContext } from '../../../ContextReducer'
+
+const { FutureMeeting, PastMeeting, CanceledMeeting } = meeting
+
+const showListItem = (status, lists) => {
+    if (status === 'future') {
+        if (lists[status][0] === undefined) {
+            return (
+                <p className='clt_list-empty-msg'>目前尚未有任何紀錄!</p>
+            )
+        }
+        return (
+            lists[status].map((e) => (<FutureMeeting meetingInfo={e} />))
+        )
+    } else if (status === 'past') {
+        if (lists[status][0] === undefined) {
+            return (
+                <p className='clt_list-empty-msg'>目前尚未有任何紀錄!</p>
+            )
+        }
+        return (
+            lists[status].map((e) => (<PastMeeting meetingInfo={e} />))
+        )
+    } else {
+        if (lists[status][0] === undefined) {
+            return (
+                <p className='clt_list-empty-msg'>目前尚未有任何紀錄!</p>
+            )
+        }
+        return (
+            lists[status].map((e) => (<CanceledMeeting meetingInfo={e} />))
+        )
+    }
+}
+
+const unclickStyle = {
+    color: '#4bb3cc',
+    backgroundColor: '#f5f5f5'
+}
+const clickedStyle = {
+    color: '#f5f5f5',
+    backgroundColor: '#4bb3cc'
+}
+
+const MeetingList = () => {
+    const [mode, setmode] = useState('future')
+    const [futureStyle, setFutureStyle] = useState(clickedStyle)
+    const [pastStyle, setPastStyle] = useState(unclickStyle)
+    const [cancelStyle, setCancelStyle] = useState(unclickStyle)
+    const context = useContext(ParamContext)
+    const handleModeToFuture = () => {
+        if (mode === 'future') return
+        else if (mode === 'past') {
+            setPastStyle(unclickStyle)
+        } else {
+            setCancelStyle(unclickStyle)
+        }
+        setFutureStyle(clickedStyle)
+        setmode('future')
+    }
+    const handleModeToPast = () => {
+        if (mode === 'past') return
+        else if (mode === 'future') {
+            setFutureStyle(unclickStyle)
+        } else {
+            setCancelStyle(unclickStyle)
+        }
+        setPastStyle(clickedStyle)
+        setmode('past')
+    }
+    const handleModeToCancel = () => {
+        if (mode === 'cancelled') return
+        else if (mode === 'past') {
+            setPastStyle(unclickStyle)
+        } else {
+            setFutureStyle(unclickStyle)
+        }
+        setCancelStyle(clickedStyle)
+        setmode('cancelled')
+    }
+    return (
+        <div className="clt_schedule-list">
+            <div className="clt_schedule-list-modes">
+                <span className="clt_schedule-list-mode" onClick={handleModeToFuture} style={futureStyle}>未來排程</span>
+                <span className="clt_schedule-list-mode" onClick={handleModeToPast} style={pastStyle}>諮詢紀錄</span>
+                <span className="clt_schedule-list-mode" onClick={handleModeToCancel} style={cancelStyle}>取消紀錄</span>
+            </div>
+            <div className="clt_schedule-list-items">
+                {showListItem(mode, context.Info.meetingsByStatus)}
+            </div>
+        </div>
+    )
+}
+
+export default MeetingList
