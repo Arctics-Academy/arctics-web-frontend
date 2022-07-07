5f8c7be5d47342eaff85d153d156a2f62f1ca540
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-07-07 09:50:26 +0100

    fix: update various issues with action button

--- src/GlobalComponents/Components/ActionButton.js
+++ src/GlobalComponents/Components/ActionButton.js
@@ -2,7 +2,7 @@
 import "./global-button.css"
 
 // Main Component
-const ActionButton = ({ label, loading=false, callback }) => {
+const ActionButton = ({ type=undefined, label, loading=false, callback }) => {
   if (label.length <= 2) {
     if (loading) {
       return (
@@ -18,7 +18,7 @@ const ActionButton = ({ label, loading=false, callback }) => {
     }
     else {
       return (
-        <button className="global__action-button global__action-button-2" onClick={callback}>
+        <button type={(type ? type : '')} className="global__action-button global__action-button-2" onClick={callback}>
           <span className="global__action-button-text">{label}</span>
         </button>
       )
@@ -39,7 +39,7 @@ const ActionButton = ({ label, loading=false, callback }) => {
     }
     else {
       return (
-        <button className="global__action-button global__action-button-4" onClick={callback}>
+        <button type={(type ? type : '')} className="global__action-button global__action-button-4" onClick={callback}>
           <span className="global__action-button-text">{label}</span>
         </button>
       )
