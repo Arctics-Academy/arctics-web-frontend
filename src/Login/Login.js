7acb75d8cd84810910da7a043f6f6aec91236ef3
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-07-07 09:20:17 +0100

    fix: fix login css issue

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -10,6 +10,8 @@ import { wrapLoginData } from "../DataProcessUtils"
 
 import MetaTags from 'react-meta-tags'
 
+import ActionButton from "../GlobalComponents/Components/ActionButton"
+
 const Login = () => {
     const [account, setAccount] = useState('')
     const [password, setPassword] = useState('')
@@ -72,38 +74,40 @@ const Login = () => {
             <MetaTags>
                 <title>登入 | Arctics</title>
             </MetaTags>
-            <div className="login_mode">
-                <div className="login_mode_tc">
-                    <label htmlFor="tc" className="login_tc">
-                        <input className="login_mode_radio" id='tc' type='radio' name='login' onChange={handleChangeMode} defaultChecked/>
-                        <span className="login_tc_text">顧問登入</span>
-                    </label>
-                </div>
-                <div className="login_mode_st">
-                    
-                    <label htmlFor="st" className="login_st">
-                        <input className="login_mode_radio" id='st' type='radio' name='login' onChange={handleChangeMode}/>
-                        <span className="login_st_text">學生登入</span>
-                    </label>
-                </div>
-            </div>
-            <div className="login_form">
-                <div className="login_title">
-                    <Lock className="login_lock_icon"/>
-                </div>
-                <div className="login_input">
-                    <div className="login_account">
-                        <input className="login_account_inputbox" value={account} onChange={evt=>{setAccount(evt.target.value)}} placeholder="電子郵件" />
-                        <p className="login_account_warn">{displayWrongAct? '電子郵件錯誤, 請重新輸入':' '}</p>
+            <div className="login_wrapper">
+                <div className="login_mode">
+                    <div className="login_mode_tc">
+                        <label htmlFor="tc" className="login_tc">
+                            <input className="login_mode_radio" id='tc' type='radio' name='login' onChange={handleChangeMode} defaultChecked/>
+                            <span className="login_tc_text">顧問登入</span>
+                        </label>
                     </div>
-                    <div className="login_password">
-                        <input type="password" minLength={8} className="login_password_inputbox" vlaue={password} onChange={evt=>{setPassword(evt.target.value)}} placeholder="密碼" />
-                        <p className="login_password_warn">{displayWrongPwd? '密碼錯誤, 請重新輸入':' '}</p>
+                    <div className="login_mode_st">
+                        
+                        <label htmlFor="st" className="login_st">
+                            <input className="login_mode_radio" id='st' type='radio' name='login' onChange={handleChangeMode}/>
+                            <span className="login_st_text">學生登入</span>
+                        </label>
                     </div>
-                    <div className="login_submit">
-                        <button className="login_submit_btn" onClick={handleLogin}>{loading? (<img className="login-loading" src={Loading}/>):'登入'}</button>
+                </div>
+                <div className="login_form">
+                    <div className="login_title">
+                        <Lock className="login_lock_icon"/>
                     </div>
-                    <p className="login_to_register">還沒有帳號嗎？<Link to='/register-identity'><span className="login_register_link">點我註冊</span></Link></p>
+                    <div className="login_input">
+                        <div className="login_account">
+                            <input className="login_account_inputbox" value={account} onChange={evt=>{setAccount(evt.target.value)}} placeholder="電子郵件" />
+                            <p className="login_account_warn">{displayWrongAct? '電子郵件錯誤, 請重新輸入':' '}</p>
+                        </div>
+                        <div className="login_password">
+                            <input type="password" minLength={8} className="login_password_inputbox" vlaue={password} onChange={evt=>{setPassword(evt.target.value)}} placeholder="密碼" />
+                            <p className="login_password_warn">{displayWrongPwd? '密碼錯誤, 請重新輸入':' '}</p>
+                        </div>
+                        <div className="login_submit">
+                            <ActionButton label="登入" loading={loading} callback={handleLogin} />
+                        </div>
+                        <p className="login_to_register">還沒有帳號嗎？ <Link to='/register-identity'><span className="login_register_link">點我註冊</span></Link></p>
+                    </div>
                 </div>
             </div>
         </div>
