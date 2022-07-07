b1e7466bb1bd0288feec618ec82eeb1b6722d526
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-07-07 08:34:58 +0100

    chore: add loader button

---
+++ src/GlobalComponents/Components/ActionButton.js
@@ -0,0 +1,52 @@
+// Stylesheet
+import "./global-button.css"
+
+// Main Component
+const ActionButton = ({ label, loading=false, callback }) => {
+  if (label.length <= 2) {
+    if (loading) {
+      return (
+        <button className="global__action-button global__action-button-2 global__action-button--loading">
+            <div className="global__action-button-div--loading">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+        </button>
+      )
+    }
+    else {
+      return (
+        <button className="global__action-button global__action-button-2" onClick={callback}>
+          <span className="global__action-button-text">{label}</span>
+        </button>
+      )
+    }
+  }
+  else {
+    if (loading) {
+      return (
+        <button className="global__action-button global__action-button-4 global__action-button--loading">
+            <div className="global__action-button-div--loading">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+        </button>
+      )
+    }
+    else {
+      return (
+        <button className="global__action-button global__action-button-4" onClick={callback}>
+          <span className="global__action-button-text">{label}</span>
+        </button>
+      )
+    }
+  }
+}
+
+// Exports
+export default ActionButton
+
